075126856a9152768cb9bd08c6e348ccb7e0d441
Douwo <874952595@qq.com>
2018-03-09 08:45:30 +0800

    去除ejs模板，以及nodejs搭建本地服务器文件

--- README.md
+++
@@ -1,1 +0,0 @@
-# CQhuoyunfengxian-QY
--- ejs/public/js/main2.js
+++
@@ -1,7 +0,0 @@
-laydate.render({
-	elem: '#date'
-});
-$(function() {
-	$('body').css('opacity', '1');
-	$('input[type="image"]').attr('draggable', 'false');
-})
